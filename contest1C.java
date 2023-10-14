7d671bde0be1e6bd9412e12e070ddfaef0a806a7
fafafa114 <37691767+fafafa114@users.noreply.github.com>
2023-10-14 14:07:50 +0300

    codestyle

--- contest1C.java
+++ contest1C.java
@@ -2,38 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class StringMatching {
+
   public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String s1 = br.readLine();
-    String s2 = br.readLine();
-    if (s1.length() > s2.length()) {
+
+    BufferedReader bufferedReader =
+        new BufferedReader(new InputStreamReader(System.in));
+
+    String substring = bufferedReader.readLine();
+    String fullString = bufferedReader.readLine();
+
+    if (substring.length() > fullString.length()) {
       return;
     }
-    int base = 237;
 
-    long hsh1 = 0;
-    long hsh2 = 0;
+    int BASE = 237;
+
+    long hashSubstring = 0;
+    long hashFullString = 0;
     long h = 1;
 
-    for (int i = 0; i < s1.length(); i++) {
-      hsh1 = hsh1 * base + s1.charAt(i);
-      hsh2 = hsh2 * base + s2.charAt(i);
-      if (i > 0)
-        h = h * base;
+    for (int i = 0; i < substring.length(); i++) {
+      hashSubstring = hashSubstring * BASE + substring.charAt(i);
+      hashFullString = hashFullString * BASE + fullString.charAt(i);
+      if (i > 0) {
+        h = h * BASE;
+      }
     }
 
     StringBuilder result = new StringBuilder();
 
-    if (hsh1 == hsh2) {
+    if (hashSubstring == hashFullString) {
       result.append("0 ");
     }
 
-    for (int i = s1.length(); i < s2.length(); i++) {
-      hsh2 = (hsh2 - s2.charAt(i - s1.length()) * h) * base + s2.charAt(i);
-
-      if (hsh1 == hsh2) {
-        result.append((i - s1.length() + 1)).append(" ");
+    for (int i = substring.length(); i < fullString.length(); i++) {
+      hashFullString =
+          (hashFullString - fullString.charAt(i - substring.length()) * h) *
+              BASE +
+          fullString.charAt(i);
+      if (hashSubstring == hashFullString) {
+        result.append((i - substring.length() + 1)).append(" ");
       }
     }
 
