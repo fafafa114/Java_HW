67642d5d71557cf6c9a871964d4f1b62db3676c4
fafafa114 <37691767+fafafa114@users.noreply.github.com>
2023-10-21 10:50:49 +0300

    codestyle

--- contest1B.java
+++ contest1B.java
@@ -1,46 +1,56 @@
 import java.util.Scanner;
 
 class Node {
-  int v;
-  Node ls, rs;
+  int value;
+  Node leftChild;
+  Node rightChild;
+
+  Node(int value) { this.value = value; }
 }
 
-public class Main {
-  public static Node insert(Node rt, int v) {
-    if (rt == null) {
-      rt = new Node();
-      rt.v = v;
-      return rt;
+public class BinaryTree {
+
+  public static Node insert(Node root, int value) {
+    if (root == null) {
+      return new Node(value);
     }
-    if (v == rt.v) return rt;
-    if (v < rt.v) {
-      rt.ls = insert(rt.ls, v);
-      return rt;
+    if (value == root.value) {
+      return root;
     }
-    rt.rs = insert(rt.rs, v);
-    return rt;
+    if (value < root.value) {
+      root.leftChild = insert(root.leftChild, value);
+    } else {
+      root.rightChild = insert(root.rightChild, value);
+    }
+    return root;
   }
 
-  public static void dfs(Node rt) {
-    if (rt == null)
+  public static void printLeaves(Node root) {
+    if (root == null) {
       return;
-    if (rt.ls == null && rt.rs == null) {
-      System.out.print(rt.v + " ");
     }
-    dfs(rt.ls);
-    dfs(rt.rs);
+    if (root.leftChild == null && root.rightChild == null) {
+      System.out.print(root.value + " ");
+    }
+    printLeaves(root.leftChild);
+    printLeaves(root.rightChild);
+  }
+
+  public static Node readInputAndBuildTree() {
+    Scanner scanner = new Scanner(System.in);
+    Node root = null;
+    while (true) {
+      int inputValue = scanner.nextInt();
+      if (inputValue == 0) {
+        break;
+      }
+      root = insert(root, inputValue);
+    }
+    return root;
   }
 
   public static void main(String[] args) {
-    Node rt = null;
-    Scanner scanner = new Scanner(System.in);
-    int x;
-    while (true) {
-      x = scanner.nextInt();
-      if (x == 0)
-        break;
-      rt = insert(rt, x);
-    }
-    dfs(rt);
+    Node root = readInputAndBuildTree();
+    printLeaves(root);
   }
 }
